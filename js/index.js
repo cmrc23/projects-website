f5c963b7502dd8ef2c40b71cc47a01103fbd6838
CharlieGroves <ctrg04@gmail.com>
2020-10-09 17:27:03 +0100

    sorted out files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementById("photosbutton").onclick = function () {
+    window.location.href= "photos.html";
+};
+
+document.getElementById("graphicsbutton").onclick = function () {
+    window.location.href= "graphics.html";
+};
+document.getElementById("codebutton").onclick = function () {
+    window.location.href= "code.html";
+};
