1a08a320641bd033c2c2ebc91a8e5838d23c96c4
Hugh Mungus <42658248+cmrc23@users.noreply.github.com>
2020-10-13 20:41:21 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ function myFunction() {
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) || !e.target.matches('.activedropbtn')) {
+    if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
